46f367716ca9faf58f7f67e2a416ba2637107fc9
Sandeepkondreddy <Sandeepkondreddy@gmail.com>
2018-05-29 09:38:37 +0530

    Sec Beat Commit-07i

--- www/js/secbeat.js
+++ www/js/secbeat.js
@@ -1,7 +1,7 @@
 var qsParm = new Array(), oldvalue = "";
 document.addEventListener("deviceready", onDeviceReady, false);
 
-function onDeviceReady() {
+/* function onDeviceReady() {
     document.addEventListener("backbutton", onBackKeyDown, false);
     $("#hiduuid").val(device.uuid);
     window.plugins.imeiplugin.getImei(callback);
@@ -46,7 +46,34 @@ function onDeviceReady() {
     function(){
         lblerr.innerHTML = "";
     });
-}
+} */
+onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+
+    // Read NDEF formatted NFC Tags
+    nfc.addNdefListener (
+        function (nfcEvent) {
+            var tag = nfcEvent.tag,
+                ndefMessage = tag.ndefMessage;
+
+            // dump the raw json of the message
+            // note: real code will need to decode
+            // the payload from each record
+            alert(JSON.stringify(ndefMessage));
+
+            // assuming the first record in the message has
+            // a payload that can be converted to a string.
+            alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+        },
+        function () { // success callback
+            alert("Waiting for NDEF tag");
+        },
+        function (error) { // error callback
+            alert("Error adding NDEF listener " + JSON.stringify(error));
+        }
+    );
+},
+
 function onBackKeyDown() {
 }
 function callback(imei) {
