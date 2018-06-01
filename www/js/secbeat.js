3889750de78b1555149fa4d19e09bbd2c99748eb
Sandeepkondreddy <Sandeepkondreddy@gmail.com>
2018-06-01 12:14:46 +0530

    Hi

--- www/js/secbeat.js
+++ www/js/secbeat.js
@@ -211,10 +211,6 @@ $(document).ready(function () {
             alert('Please Scan Tag.');
 			//window.location.href = 'TallySheet.html?user=' + btoa($("#hidusrid").val()) + '&trkid=' + btoa($("#hidTruckId").val()) + '&trkno=' + btoa($("#txttruckno").val().trim()) + '&loctype=' + btoa($("#hidloctype").val()) + '';
         }
-		else if($("#txtloc").val() == "")
-		{
-			 alert('Location details not found.');
-		}
         else
         {
             $btn.html("<i class='fa fa-spinner fa-spin'></i>data is submitting please wait...");
@@ -404,7 +400,7 @@ function GetDeviceStatus()
         type: "POST",
 	    url: 'http://apps.kpcl.com/KPCTSDS/api/Account/GetDeviceStatus',
         //url: 'http://202.83.27.199/KPCTSDS/api/Account/GetDeviceStatus',
-	    //url: 'http://182.72.244.25/KPCTSDS/api/Account/GetDeviceStatus',
+	//url: 'http://182.72.244.25/KPCTSDS/api/Account/GetDeviceStatus',
         dataType: "json",
         data: Adddata,
         async: false,
